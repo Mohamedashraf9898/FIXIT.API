207417ebc30f1540428772e6dc3d465ee658ad66
gehadmaghawry17 <gehadmaghawry17@gmail.com>
2025-10-24 14:51:07 +0300

    resolve rrror

---
+++ FIXIT.API/Controllers/ServiceRequestController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIXIT.API.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        //injection 
+        private readonly
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult> GetMyServiceRequest()
+        //{
+        //    var client = client.await _dbContext.Clients.FindAsync(Id);
+        //    if (client == null)
+        //        return NotFound("Client not found.");
+        //    var requests = await _dbContext.ServiceRequest
+        //                    .
+
+                        //.Include(r => r.CraftsMan)
+                        //.Include(r => r.Service)
+                        //.Where(r => r.Id ==Id)
+                        //.OrderByDescending(r => r.RequestedAt)
+                        //.Select(r => new
+                        //{
+                        //    r.RequestId,
+                        //    r.Description,
+                        //    r.Status,
+                        //    r.RequestedAt,
+                        //    r.CostEstimate,
+                        //    CraftsMan = r.CraftsMan.FName,
+                        //})
+                        //.ToListAsync();
+
+    //    return Ok(requests); 
+    //}
+}
+}

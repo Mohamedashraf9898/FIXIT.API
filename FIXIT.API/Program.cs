8c4f5bb9c8c4271659ae4fc031ebbfd1900cabd2
Mohamed Ashraf <mohamedashraf171998@gmail.com>
2025-10-27 11:49:36 +0300

    Update-Database Auto

--- FIXIT.API/Program.cs
+++ FIXIT.API/Program.cs
@@ -5,13 +5,14 @@ using FIXIT.BLL.Services;
 using FIXIT.DAL;
 using FIXIT.DAL.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using CraftsManService = FIXIT.BLL.Services.CraftsManService;
 
 namespace FIXIT.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +30,18 @@ namespace FIXIT.API
             builder.Services.AddScoped<ICraftsManService, CraftsManService>();
 
 			var app = builder.Build();
+            var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<FixItDbContext>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())

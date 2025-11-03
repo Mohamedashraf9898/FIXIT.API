0a3ff490f2762ae13f70185d38228c6ee15c79cd
osama maher <osamamaher7777@gmail.com>
2025-11-03 20:24:01 +0200

    Rename DeleteCraftsMan method to Delete

--- FIXIT.API/Controllers/ClientController.cs
+++ FIXIT.API/Controllers/ClientController.cs
@@ -60,7 +60,7 @@ namespace FIXIT.API.Controllers
         }
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult DeleteCraftsMan(int id)
+        public IActionResult Delete(int id)
         {
             
 

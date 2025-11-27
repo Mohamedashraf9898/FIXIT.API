fc580ec8ae3e4807d3d881d7b98c2c4d01c65f6a
Osama Maher <osamamaher7777@gmail.com>
2025-11-27 23:06:01 +0200

    Adding dashboard

--- FIXIT.BLL/DTOs/CraftsmanDTOs/UpdateCraftsManDto.cs
+++ FIXIT.BLL/DTOs/CraftsmanDTOs/UpdateCraftsManDto.cs
@@ -18,6 +18,6 @@ namespace FIXIT.BLL.DTOs.CraftsmanDTOs
 		public int ExperienceOfYears { get; set; }
 		public decimal HourlyRate { get; set; }
 
-
-	}
+        public bool IsVerified { get; set; }
+    }
 }

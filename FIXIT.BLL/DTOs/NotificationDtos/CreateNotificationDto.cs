6bd97cd8a3212b1915cf3345e22382cdb394b52b
Mohamed Ashraf <mohamedashraf171998@gmail.com>
2025-11-28 23:38:18 +0200

    AddFinalAmountToDto

--- FIXIT.BLL/DTOs/NotificationDtos/CreateNotificationDto.cs
+++ FIXIT.BLL/DTOs/NotificationDtos/CreateNotificationDto.cs
@@ -16,6 +16,7 @@ namespace FIXIT.BLL.DTOs.NotificationDtos
         public string Message { get; set; }
         public int? ClientId { get; set; }
         public bool IsRead { get; set; }
+        public decimal? FinalAmount { get; set; }
 
         public int? CraftsManId { get; set; }
         public NotificationSenderType SenderType { get; set; }
